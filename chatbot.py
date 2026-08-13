9e37ada8bd5c61600881e12d9e88b34c21cde80e
jpvinisha91-arch <jpvinisha91@jnn.edu.in>
2026-08-14 01:20:08 +0530

    Task 4 completed
    Completed Basic Chatbot using Python with if-elif, functions, loops, and user input.

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+def chatbot():
+    print("Chatbot: Hi! How can I help you?")
+
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("Chatbot: Hi!")
+
+        elif user_input == "how are you":
+            print("Chatbot: I'm fine, thanks!")
+
+        elif user_input == "bye":
+            print("Chatbot: Goodbye!")
+            break
+
+        else:
+            print("Chatbot: Sorry, I don't understand.")
+
+chatbot()
